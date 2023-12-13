1b8dfe197a789ee05a2298dc693e7fd9539bf89f
Seiryu Teruya <e235720@ie.u-ryukyu.ac.jp>
2023-12-13 15:54:02 +0900

    third commit

--- app/src/main/java/jp/ac/uryukyu/ie/e235720/Report5.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235720/Report5.java
@@ -2,27 +2,7 @@ package jp.ac.uryukyu.ie.e235720;
 
 public class Report5 {
     public static void main(String[] args) {
-        
-      
-       
-    
-    
-    try {
-       
-        String str = null;
-
-        
-        System.out.println("NullPointerException occurred.");
-
-        
-        System.out.println("Exception message: " + new NullPointerException().getMessage());
-
-        // NullPointerExceptionが発生する行
-        System.out.println(str.length());
-    } catch (NullPointerException e) {
-        // 例外処理
-        System.err.println("Caught NullPointerException: " + e.getMessage());
+    String str = "壱百満";
+    int value = Integer.parseInt(str);
     }
-    
-}
 }
