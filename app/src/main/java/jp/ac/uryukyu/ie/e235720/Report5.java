c895b43c9dcb97c32cd84ac81aa42ff0cd03a2bd
Seiryu Teruya <e235720@ie.u-ryukyu.ac.jp>
2023-12-13 12:56:21 +0900

    first commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235720/Report5.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e235720;
+
+public class Report5 {
+    public static void main(String[] args) {
+        // 条件1: String型変数 str を宣言し、初期値として null を代入する。
+        String str = null;
+
+        // 条件2: str.length() の内容を表示しようとする。
+        System.out.println(str.length());
+    }
+}
